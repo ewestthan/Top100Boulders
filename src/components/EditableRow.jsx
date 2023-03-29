6bab65d67c187bd88b3ca767b585a606a8b0c87c
Ethan Patrick West <ethan.west@uvm.edu>
2023-03-28 20:33:12 -0400

    table is editable and draggable, need to fix checkboxes

---
+++ src/components/EditableRow.jsx
@@ -0,0 +1,19 @@
+
+import {Form} from 'antd';
+import React from 'react';
+
+const EditableContext = React.createContext(null);
+const EditableRow = ({ index, ...props }) => {
+  
+  const [form] = Form.useForm();
+  
+  return (
+    <Form form={form} component={false}>
+      <EditableContext.Provider value={form}>
+        <tr {...props} />
+      </EditableContext.Provider>
+    </Form>
+  );
+};
+
+export default EditableRow;
---
+++ src/components/MainTable.jsx
@@ -0,0 +1,396 @@
+import { Button, Form, Input, Popconfirm, Table, Checkbox, InputNumber, Typography  } from 'antd';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { MenuOutlined, CheckCircleFilled, CloseCircleOutlined} from '@ant-design/icons';
+import { DndContext } from '@dnd-kit/core';
+import {
+  arrayMove,
+  SortableContext,
+  useSortable,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+
+const Row = ({ children, ...props }) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    setActivatorNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: props['data-row-key'],
+  });
+  const style = {
+    ...props.style,
+    transform: CSS.Transform.toString(
+      transform && {
+        ...transform,
+        scaleY: 1,
+      },
+    )?.replace(/translate3d\(([^,]+),/, 'translate3d(0,'),
+    transition,
+    ...(isDragging
+      ? {
+          position: 'relative',
+          zIndex: 9999,
+        }
+      : {}),
+  };
+  return (
+    <tr {...props} ref={setNodeRef} style={style} {...attributes}>
+      {React.Children.map(children, (child) => {
+        if (child.key === 'sort') {
+          return React.cloneElement(child, {
+            children: (
+              <MenuOutlined
+                ref={setActivatorNodeRef}
+                style={{
+                  touchAction: 'none',
+                  cursor: 'move',
+                }}
+                {...listeners}
+              />
+            ),
+          });
+        }
+        return child;
+      })}
+    </tr>
+  );
+};
+
+const EditableCell = ({
+  editing,
+  dataIndex,
+  title,
+  inputType,
+  record,
+  index,
+  children,
+  ...restProps
+}) => {
+  const inputNode = inputType === 'boolean' ? <Checkbox defaultChecked={record[dataIndex]}/> : <Input />;
+  return (
+    <td {...restProps}>
+      {editing ? (
+
+        <Form.Item
+          name={dataIndex}
+          style={{
+            margin: 0,
+          }}
+          rules={[
+            {
+              required: true,
+              message: `Please Input ${title}!`,
+            },
+          ]}
+        >
+          {inputNode}
+        </Form.Item>
+      ) : (
+        children
+      )}
+    </td>
+  );
+};
+
+const MainTable = () => {
+
+  const isTrue = (text) => text;
+
+  const [dataSource, setDataSource] = useState([
+    {
+      key: '1',
+      rank: '1',
+      grade: 'V11',
+      name: 'Western Gold',
+      location: "Southeast",
+      uncontrived: true,
+      obviousStart: false,
+      greatRock: true,
+      flatLanding: false,
+      tall: true,
+      beautifulSetting: true,
+      sent: false,
+    },
+    {
+      key: '2',
+      rank: '2',
+      grade: 'V13',
+      name: 'Kintsugi',
+      location: "Red Rocks",
+      uncontrived: false,
+      obviousStart: false,
+      greatRock: true,
+      flatLanding: true,
+      tall: false,
+      beautifulSetting: true,
+      sent: false,
+    }
+  ]);
+
+  const defaultColumns = [
+    {
+      key: 'sort'
+    },
+    {
+      title: 'Rank',
+      dataIndex: 'rank',
+      key: 'rank',
+    },
+    {
+      title: 'Grade',
+      dataIndex: 'grade',
+      key: 'grade',
+      editable: true,
+      inputType: "number"
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      editable: true
+    },
+    {
+      title: 'Location',
+      dataIndex: 'location',
+      key: 'location',
+      editable: true,
+    },
+    {
+      title: 'Uncontrived',
+      dataIndex: 'uncontrived',
+      key: 'uncontrived',
+      editable: true,
+      inputType: 'boolean',
+      render : (text) => text ? <CheckCircleFilled /> : <CloseCircleOutlined />,
+    },
+    {
+      title: 'Obvious Start',
+      dataIndex: 'obviousStart',
+      key: 'obviousStart',
+      editable: true,
+      inputType: 'boolean',
+      render : (text) => text ? <CheckCircleFilled /> : <CloseCircleOutlined />,
+    },
+    {
+      title: 'Great Rock',
+      dataIndex: 'greatRock',
+      key: 'greatRock',
+      editable: true,
+      inputType: 'boolean',
+      render : (text) => text ? <CheckCircleFilled /> : <CloseCircleOutlined />,
+    },
+    {
+      title: 'Flat Landing',
+      dataIndex: 'flatLanding',
+      key: 'flatLanding',
+      editable: true,
+      inputType: 'boolean',
+      render : (text) => text ? <CheckCircleFilled /> : <CloseCircleOutlined />,
+    },
+    {
+      title: 'Tall',
+      dataIndex: 'tall',
+      key: 'tall',
+      editable: true,
+      inputType: 'boolean',
+      render : (text) => text ? <CheckCircleFilled /> : <CloseCircleOutlined />,
+    },
+    {
+      title: 'Beautiful Setting',
+      dataIndex: 'beautifulSetting',
+      key: 'beautifulSetting',
+      editable: true,
+      inputType: 'boolean',
+      render : (text) => text ? <CheckCircleFilled /> : <CloseCircleOutlined />,
+
+    },
+    {
+      title: 'Sent',
+      dataIndex: 'sent',
+      key: 'sent',
+      editable: true,
+      inputType: 'boolean',
+      render : (_, text) => {
+        console.log(text.sent)
+        return text.sent ? (<CheckCircleFilled />) : (<CloseCircleOutlined />)
+      },
+    },
+    {
+      title: 'operation',
+      dataIndex: 'operation',
+      render: (_, record) => {
+        const editable = isEditing(record);
+        return editable ? (
+          <span>
+            <Typography.Link
+              onClick={() => handleSave(record.key)}
+              style={{
+                marginRight: 8,
+              }}
+            >
+              Save
+            </Typography.Link>
+            <Popconfirm title="Sure to cancel?" onConfirm={handleCancel}>
+              <a>Cancel</a>
+            </Popconfirm>
+          </span>
+        ) : (
+          <span>
+            <Typography.Link disabled={editingKey !== ''} onClick={() => handleEdit(record)}>
+              Edit
+            </Typography.Link>
+            <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.key)}>
+              <a> Delete</a>
+            </Popconfirm>
+          </span>
+        );
+      },
+    },
+  ];
+
+  const [form] = Form.useForm();
+  const [editingKey, setEditingKey] = useState('');
+  const isEditing = (record) => record.key === editingKey;
+  
+  const [count, setCount] = useState(2);
+
+
+  const handleEdit = (record) => {
+    form.setFieldsValue({
+      rank: '',
+      grade: 'V',
+      name: '',
+      location: "",
+      uncontrived: false,
+      obviousStart: false,
+      greatRock: false,
+      flatLanding: false,
+      tall: false,
+      beautifulSetting: false,
+      sent: '',
+      ...record,
+    });
+    setEditingKey(record.key);
+  };
+
+  const handleCancel = () => {
+    setEditingKey('');
+  };
+
+  const handleSave = async (key) => {
+    try {
+      const row = await form.validateFields();
+      const newData = [...dataSource];
+      const index = newData.findIndex((item) => key === item.key);
+      if (index > -1) {
+        const item = newData[index];
+        newData.splice(index, 1, {
+          ...item,
+          ...row,
+        });
+        setDataSource(newData);
+        setEditingKey('');
+      } else {
+        newData.push(row);
+        setDataSource(newData);
+        setEditingKey('');
+      }
+    } catch (errInfo) {
+      console.log('Validate Failed:', errInfo);
+    }
+  };
+
+  const handleDelete = (key) => {
+    const newData = dataSource.filter((item) => item.key !== key);
+    setDataSource(newData);
+  };
+
+  const handleAdd = () => {
+    const newData = {
+      key: count,
+      rank: 'X',
+      grade: 'X',
+      name: 'X',
+      location: "X",
+      uncontrived: false,
+      obviousStart: false,
+      greatRock: false,
+      flatLanding: false,
+      tall: false,
+      beautifulSetting: false,
+      sent: false,
+    };
+    setDataSource([...dataSource, newData]);
+    setCount(count + 1);
+  };
+
+  const onDragEnd = ({ active, over }) => {
+    if (active.id !== over?.id) {
+      setDataSource((previous) => {
+        const activeIndex = previous.findIndex((i) => i.key === active.id);
+        const overIndex = previous.findIndex((i) => i.key === over?.id);
+        return arrayMove(previous, activeIndex, overIndex);
+      });
+    }
+  };
+
+  const mergedColumns = defaultColumns.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+        inputType: col.inputType === 'boolean' ? 'boolean' : 'text',
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editing: isEditing(record),
+      }),
+    };
+  });
+
+
+  return (
+    <div>
+      <Button
+        onClick={handleAdd}
+        type="primary"
+        style={{
+          marginBottom: 16,
+        }}
+      >
+        Add a row
+      </Button>
+      <DndContext onDragEnd={onDragEnd}>
+        <SortableContext
+          // rowKey array
+          items={dataSource.map((i) => i.key)}
+          strategy={verticalListSortingStrategy}
+        >
+          <Form form={form} component={false}>
+          <Table
+            components={{
+              body: {
+                row: Row,
+                cell: EditableCell,
+              },
+            }}
+            rowKey="key"
+            bordered
+            dataSource={dataSource}
+            columns={mergedColumns}
+            rowClassName="editable-row"
+          />
+          </Form>
+        </SortableContext>
+      </DndContext>
+    </div>
+  );
+};
+export default MainTable;
